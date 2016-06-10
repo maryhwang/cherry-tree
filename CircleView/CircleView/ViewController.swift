6970441220c4cc48c13bae75cde5f427d5b2b1c4
maryhwang <hong_mary_wang@yahoo.com>
2016-06-09 17:18:58 -0700

    initial checkin
    initial check in

---
+++ CircleView/CircleView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CircleView
+//
+//  Created by James Dempsey on 1/15/15.
+//  Copyright (c) 2015 Example Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
